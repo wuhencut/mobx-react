642feaad53e18e400c75bd5a30abe15cf77a4c63
stone <stone_rivers@163.com>
2021-04-22 22:44:07 +0800

    双向绑定实现

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-
-function App() {
-  return (<div>123</div>);
+import { inject, observer } from "mobx-react";
+import React from "react";
+@inject("count") @observer
+class App extends React.Component {
+  render() {
+    const { count } = this.props;
+    return <div>
+      <button onClick={() => count.minus()}>-</button>
+      <div>{count.num}</div>
+      <button onClick={() => count.plus()}>+</button>
+    </div>;
+  }
 }
-
 export default App;
--- src/store/count.js
+++ src/store/count.js
@@ -1,10 +1,13 @@
-import {observable, action} from "mobx"
+import {observable, action, makeAutoObservable} from "mobx"
  class count {
-   @observable num = 1;
+  @observable num = 1;
    @action plus (){
+     console.log(11)
      this.num ++;
    }
    @action minus () {
      this.num --;
    }
  }
+
+ export default new count()
